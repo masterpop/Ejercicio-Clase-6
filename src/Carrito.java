32c0013f211bc5fd2402a941c3a755a521b9d5df
masterpop <isadsoria@hotmail.es>
2023-03-17 00:44:56 -0300

    Ejercicio numero 6 y 7 !!! Mejorado con try y catch

--- src/Carrito.java
+++ src/Carrito.java
@@ -2,25 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Carrito {
-    private List<Producto> productos = new ArrayList<>();
+    private Producto[] productos = new Producto[3];
+    private int cantidadProductos = 0;
     private Descuento descuento;
 
     public void agregarProducto(Producto producto) {
-        productos.add(producto);
+        if (cantidadProductos < 3) {
+            productos[cantidadProductos] = producto;
+            cantidadProductos++;
+        }
     }
 
     public int precio() {
         int total = 0;
-        for (Producto producto : productos) {
-            total += producto.getPrecio();
+        for (int i = 0; i < cantidadProductos; i++) {
+            total += productos[i].getPrecio();
         }
         if (total == 0) {
-            throw new RuntimeException("No se puede aplicar un descuento a un carrito de precio 0");
+            throw new IllegalArgumentException("El precio total del carrito es cero");
         }
         if (descuento != null) {
             int precioConDescuento = descuento.aplicarDescuento(total);
             if (precioConDescuento < 0) {
-                throw new RuntimeException("El descuento resulta en un monto negativo");
+                throw new IllegalStateException("El descuento produce un resultado negativo");
             }
             return precioConDescuento;
         } else {
@@ -32,4 +36,3 @@ public class Carrito {
         this.descuento = descuento;
     }
 }
-
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
-        // Crear productos
-        Producto jabonEnPolvo = new Producto(0, "jabón en polvo");
-        Producto esponjas = new Producto(0, "esponjas");
-        Producto chocolates = new Producto(0, "chocolates");
 
-        // Crear carrito y agregar productos
-        Carrito carrito = new Carrito();
-        carrito.agregarProducto(jabonEnPolvo);
-        carrito.agregarProducto(esponjas);
-        carrito.agregarProducto(chocolates);
+    public class Main {
+        public static void main(String[] args) {
+            // Crear productos
+            Producto jabonEnPolvo = new Producto(0, "jabón en polvo");
+            Producto esponjas = new Producto(2, "esponjas");
+            Producto chocolates = new Producto(0, "chocolates");
 
-        // Calcular precio sin descuento
-        System.out.println("Precio sin descuento: " + carrito.precio());
+            // Crear carrito y agregar productos
+            Carrito carrito = new Carrito();
+            carrito.agregarProducto(jabonEnPolvo);
+            carrito.agregarProducto(esponjas);
+            carrito.agregarProducto(chocolates);
 
-        // Aplicar descuento fijo
-        carrito.setDescuento(new DescuentoFijo(20));
-        System.out.println("Precio con descuento fijo: " + carrito.precio());
+            try {
+                // Calcular precio sin descuento
+                System.out.println("Precio sin descuento: " + carrito.precio());
 
-        // Aplicar descuento porcentaje
-        carrito.setDescuento(new DescuentoPorcentaje(0.2));
-        System.out.println("Precio con descuento porcentaje: " + carrito.precio());
+                // Calcular precio con descuento fijo
+                carrito.setDescuento(new DescuentoFijo(20));
+                System.out.println("Precio con descuento fijo: " + carrito.precio());
 
-        // Aplicar descuento porcentaje con tope
-        carrito.setDescuento(new DescuentoPorcentajeConTope(0.5, 20));
-        System.out.println("Precio con descuento porcentaje con tope: " + carrito.precio());
+                // Aplicar descuento porcentaje
+                carrito.setDescuento(new DescuentoPorcentaje(0.1));
+                System.out.println("Precio con descuento porcentaje: " + carrito.precio());
+
+                // Aplicar descuento porcentaje con tope
+                carrito.setDescuento(new DescuentoPorcentajeConTope(0.1, 30));
+                System.out.println("Precio con descuento porcentaje con tope: " + carrito.precio());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+            } catch (IllegalStateException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
     }
-}
